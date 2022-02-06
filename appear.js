c816892508f9940c88bcca9e31afcbd72afd98b3
Arty2001 <64709386+Arty2001@users.noreply.github.com>
2022-02-06 18:48:12 -0500

    Add files via upload

---
+++ appear.js
@@ -0,0 +1,5 @@
+var showText = function (target, message, index, interval) {   
+    if (index < message.length) {
+    $(target).append(message[index++]);
+    setTimeout(function () { showText(target, message, index, interval); }, interval);
+    }
